903d408bf2f82be3137eeb99d1c1aacad3ec59b0
gatan45 <joewenzel45@gmail.com>
2017-08-04 12:41:36 -0400

    made some minor changes to the logic

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,4 @@
 var topics = ["dogs", "cats", "bears", "john cena", "doge", "futurama", "birds", "computers", "hacking", "marvel", "travel"];
-var key = "97be7d60a4e14ba2adcdba33e5875010";
-var actualGifs = [];
-var stills = []
 
 function addButton(name){
   var $buttonToAdd = $("<button>");
@@ -13,16 +10,14 @@ function addButton(name){
 
 function addGifs(name){
   $("#gifsArea").empty();
-  actualGifs = [];
-  stills = []
+  var key = "97be7d60a4e14ba2adcdba33e5875010";
   var query = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + name +  "&limit=10&offset=0&rating=PG-13&lang=en";
   $.get(query).done(function(response){
     for(var i = 0; i < response.data.length; i++){
       var gifToAppend = $("<div>").addClass("gif");
-      var url = response.data[i].images.fixed_height_still.url
-      stills.push(url);
-      actualGifs.push(response.data[i].images.fixed_height.url)
-      gifToAppend.append($("<img>").attr({src: url,"data-state": 0, "data-number": i}));
+      var stillUrl = response.data[i].images.fixed_height_still.url;
+      var gifUrl = response.data[i].images.fixed_height.url;
+      gifToAppend.append($("<img>").attr({src: stillUrl, "data-toggleImg": gifUrl}));
       gifToAppend.append($("<p>").text("Rating: " + response.data[i].rating));
       $("#gifsArea").append(gifToAppend);
     }
@@ -30,14 +25,9 @@ function addGifs(name){
 }
 
 function toggleGif(currentGif){
-  if(currentGif.attr("data-state") === "0"){
-    currentGif.attr("data-state", 1);
-    currentGif.attr("src", actualGifs[currentGif.attr("data-number")]);
-  }
-  else{
-    currentGif.attr("data-state", 0);
-    currentGif.attr("src", stills[currentGif.attr("data-number")]);
-  }
+  var originalState = currentGif.attr("src");
+  currentGif.attr("src", currentGif.attr("data-toggleImg"));
+  currentGif.attr("data-toggleImg", originalState);
 }
 
 $(document).ready(function(){
